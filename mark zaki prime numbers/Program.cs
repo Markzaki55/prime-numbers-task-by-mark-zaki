5a09a0565ec21e268b23ebe00cf666157ee68ad4
mark zaki <120737045+markzaki77@users.noreply.github.com>
2022-12-22 18:39:52 +0200

    Add files via upload

---
+++ mark zaki prime numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first num:");
+        int first_num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the secound num:");
+        int secound_num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("The prime numbers between {0} and {1} are:", first_num, secound_num);
+        for (int i = first_num; i <= secound_num; i++)
+        {
+            if (prime_tester(i))
+            {
+                Console.Write(i + " ");
+            }
+        }
+        }
+
+        static bool prime_tester(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
